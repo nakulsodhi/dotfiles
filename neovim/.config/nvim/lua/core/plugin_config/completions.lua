753bf2c68ccda5aefa1d4b889b7a1387ab934a1e
Nakul S <nakulsoup@protonmail.com>
2023-07-13 19:51:28 -0500

    dotfile sync

--- neovim/.config/nvim/lua/core/plugin_config/completions.lua
+++ neovim/.config/nvim/lua/core/plugin_config/completions.lua
@@ -25,7 +25,7 @@ cmp.setup({
             if cmp.visible() then
                 cmp.select_next_item()
             elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+        --vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
         -- idk what this elseif condition does lmao copied this shit off nvchad plugins.configs.cmp
             else
                 fallback()
